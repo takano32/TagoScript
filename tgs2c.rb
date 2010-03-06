5322a91aa0664f3013377c997d9ef3de2200e249
TAKANO Mitsuhiro <takano32@gmail.com>
2010-03-06 22:20:11 +0900

    invalid じゃねーか　クソが！！！

--- tgs2c.rb
+++ tgs2c.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
-
 require 'rexml/document'
 
-#doc = REXML::Document.new(File.read('example.tgs'))
-doc = REXML::Document.new(File.read('demo2.tgs'))
-p doc.root, doc.root.class
+doc = REXML::Document.new(File.read('demo1.tgs'))
+
+puts doc
+def convert(elem)
+	p elem
+	elem.each_element do |elem|
+		convert(elem)
+	end
+end
+
+convert(doc.root)
+
