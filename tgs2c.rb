747e8f732d9493187d59bcb7e74692d27b61e76d
TAKANO Mitsuhiro <takano32@gmail.com>
2010-03-06 17:44:02 +0900

    demo{1,2,3}.tgs, example.tgs ファイルを追加

--- tgs2c.rb
+++ tgs2c.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'rexml/document'
+
+#doc = REXML::Document.new(File.read('example.tgs'))
+doc = REXML::Document.new(File.read('demo2.tgs'))
+p doc.root, doc.root.class
