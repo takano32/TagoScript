046038f67d30018f67b2ee4f49fbf5ec8c51bd33
ujihisa <ujihisa@gmail.com>
2010-03-06 17:37:52 -0800

    Indentation

--- tgs2c.rb
+++ tgs2c.rb
@@ -5,10 +5,10 @@ doc = REXML::Document.new(File.read('demo1.tgs'))
 
 puts doc
 def convert(elem)
-	p elem
-	elem.each_element do |elem|
-		convert(elem)
-	end
+  p elem
+  elem.each_element do |elem|
+    convert(elem)
+  end
 end
 
 convert(doc.root)
