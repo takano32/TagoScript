77c46b22678ec32c93f11294aa62ddccdb2e23b0
TAKANO Mitsuhiro <takano32@gmail.com>
2010-06-25 18:22:47 +0900

    add instance variable to pre or post process

--- tgs2c.rb
+++ tgs2c.rb
@@ -4,11 +4,51 @@ require 'rexml/document'
 class Translator
 	def initialize(script)
 		@script = script
+    @block_level = 0
 	end
 	
 	def self.parse(s)
 		doc = REXML::Document.new(s)
 		ret = Translator.new(doc.root)
+	end
+
+	def translate(elem = @script)
+		return unless elem
+		  case elem.name
+      when /TagoScript/i
+        puts "/* TagoScript */"
+      when 'include'
+        #p elem.methods.sort
+        puts "#include <#{elem.attribute('libr')}>"
+      when 'int'
+        if attr_main = elem.attribute('main') then
+          puts "int main(#{attr_main}){" if attr_main
+          @block_level += 1
+          translate(elem.next_element)
+        end
+        if attr_targ = elem.attribute('targ') then
+          indent = "    " * @block_level
+          attr_prop = elem.attribute('prop')
+          prop = attr_prop.to_s
+          prop = ('null' == prop) ? 0 : prop
+          puts indent + "#{attr_targ} = #{prop}"
+        end
+
+      when 'if'
+      else
+        p elem
+      end
+		elem.each_element do |elem|
+      @pre = true
+      @post = false
+      translate(elem)
+      @pre = false
+      @post = false
+			translate(elem)
+      @pre = false
+      @post = true
+      translate(elem)
+		end
 	end
 	
 	def dump(elem = @script)
@@ -22,7 +62,7 @@ end
 
 if $0 == __FILE__
 	xltor = Translator.parse(File.read('demo1.tgs'))
-	xltor.dump
+	xltor.translate
 end
 
 
